4cd4cf6bd6a82f104b3a100ed74fd90f159d73d8
Devender_kumar <kumar.devender2525@gmail.com>
2022-08-25 13:14:04 +0530

    Constructor chaining

--- OOPS/Super.java
+++ OOPS/Super.java
@@ -1,4 +1,22 @@
 
 public class Super {
+    public static void main(String[] args) {
+        
+    }
+    public static class parentClass{
+        int i;
+        int j;
 
+        parentClass(int i,int j){
+            this.i = i;
+            this.j = j;
+        }
+    }
+    public static class derivedClass extends parentClass{
+        int k;
+        derivedClass(int i,int j,int k){
+            super(i,j);
+            this.k = k;
+        }
+    }
 }
