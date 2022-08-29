9dcef7a37c04a6ae851f7ec5bee173a2c4d18007
Devender_kumar <kumar.devender2525@gmail.com>
2022-08-29 13:49:18 +0530

    DP3 Strategy Design Pattern

---
+++ LLD/strategyDesignPattern/Test.java
@@ -0,0 +1,19 @@
+package strategyDesignPattern;
+
+import strategyDesignPattern.kicks.*;
+import strategyDesignPattern.punches.*;
+import strategyDesignPattern.wrestles.*;
+
+public class Test {
+	public static void main(String[] args) {
+		Fighter fighter = new Fighter();
+		
+		fighter.kb = new JumpingKick();
+		fighter.pb = new Hook();
+		fighter.wb = new Choke();
+		
+		fighter.kick();
+		fighter.punch();
+		fighter.wrestle();
+	}
+}
