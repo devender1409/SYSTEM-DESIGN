a3ecbdae276b4f13c79560be3b8f9bf45a3db063
Devender_kumar <kumar.devender2525@gmail.com>
2022-08-31 11:44:50 +0530

    Problem

---
+++ LLD/commandDesignPattern/Problem/Test.java
@@ -0,0 +1,11 @@
+package commandDesignPattern.Problem;
+
+public class Test {
+	public static void main(String[] args) {
+		RemoteControl rc = new RemoteControl();
+		rc.btn1click();
+		rc.btn2click();
+		rc.btn3click();
+		rc.btn4click();
+	}
+}
